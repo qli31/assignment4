e1612e8523990babc24867d079533aeab6a21c57
qli31 <35783462+qli31@users.noreply.github.com>
2018-03-25 13:58:20 -0400

    new update

--- casey.php
+++ casey.php
@@ -3,7 +3,7 @@
 	<head>
 		<meta charset="utf-8">
 		<meta name="viewport" content="width=device-width, initial-scale=1.0">
-		<title>Casey A. Brentnall, Assignment 3</title>
+		<title>Casey A. Brentnall, Assignment 4</title>
 		<!-- Designer include stylesheet-->
 		<link href="css/styles.css" rel="stylesheet">
 		<!-- links for fontface -->
@@ -36,11 +36,16 @@
 		        <p>Hello! My name is Casey Adam Brentnall. I am a sophomore attending the the University of Rochester and my major is computer science.</p>
 
 				<h2>Work Experience</h2>
+				<ul>
+					<li>work as a Standardized Patient in a Suicide Prevention Training program at URMC</li>
+					<li>worked as a cashier at BJ's Wholesale Club, and Family Dollar</li>
+					
+				</ul> 
 
-				<p>Currently, I work as a Standardized Patient in a Suicide Prevention Training program at URMC. In this program, I've developed a character who is at risk for suicide, and I work with physicians and psychologists, helping them learn how to identify and treat depression and suicide. In the past, I've worked as a cashier at BJ's Wholesale Club, and Family Dollar. At these jobs, I both ran a cash register and stocked, cleaned, and handled returns. I've also worked summers as an ice cream scooper and cook at Friendly's. I learned how to make a variety of simple food items, and was in charge of cleaning the grill area thoroughly at the end of every shift.</p>
+				
 
 				<h2>Course Work</h2>
-				
+				<h3>Not heavy work load right now, but definitely a big plan ahead.</h3>
 				<p>When I enrolled at the University of Rochester, I had planned on pursuing a double major in Computer Science and English. Because of this, my previous coursework is a pretty even mix of CSC and ENG courses. I've taken classes in acting, directing, dialect training, and in theatrical performance. On the computer science side, I've taken Data Structures and Algorithms, Formal Computation and Systems, and all the required math courses. This semester, I'm taking Introduction to Artificial Intelligence, my next step in the computer science track. I'm also taking Linear Algebra and Differential Equations, my first math class after calculus. I'm taking this class, and lastly I'm taking Musical Theatre Workshop, a course in preparing scenework and audition materials for musical theatre actors.</p>
 
 
---
+++ post_survey.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Thank you, Assignment 4</title>
+	<link href="css/styles.css" rel="stylesheet">
+</head>
+	<body>
+	
+	<div class="header">
+		
+		<div class="name">Thank you!</div>
+	    <?php
+	  		include "includes/nav.php";
+	  	?>
+
+    </div>
+    <p>Thank you for filling out the survey!</p>
+            
+    
+
+	<?php
+  		include "includes/footer.php";
+  	?>
+
+	</body>
+</html>
